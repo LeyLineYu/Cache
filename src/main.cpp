56bc39ec327d14d1269a369e4ecfb7a27300c496
Dark_dragon1296 <nikita2008.volovod@gmail.com>
2026-09-10 00:19:01 +0300

    C++ test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,27 @@
 
 void printList(std::list<int>& myList);
 
+int is_cache_hit(std::list<int>& list, int value) {
+  for (int i : list) {
+    if (i == value) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int *lru_cache(std::list<int>& list, int value) {
+  int is_hit = is_cache_hit(list, value);
+
+  if (is_hit != NULL)
+    return NULL;
+
+
+  return NULL;
+}
+
 int main() {
+/*
   std::cout << "Hello World!" << std::endl;
   std::list myList{10, 5, 5, 3, 1, -1, 4, 5, 6};
   printList(myList);
@@ -21,6 +41,14 @@ int main() {
   std::advance(itEnd, 9);
   myList.erase(itBegin, itEnd);
   printList(myList);
+  return 0;
+*/
+
+  std::list myList{10, 5, 5, 3, 1, -1, 4, 5, 6};
+  int x = is_cache_hit(myList, 100);
+
+  std::cout << x << std::endl;
+
   return 0;
 }
 
